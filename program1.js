66f050a75a93f72c1ac2960ffd7787ec95811e6f
Mikhail Pisarenko <125873405+mikhail-pisarenko@users.noreply.github.com>
2024-09-24 13:14:57 +0200

    Create program1.js

---
+++ program1.js
@@ -0,0 +1,15 @@
+// Simple function to simulate user login
+function login(username, password) {
+    if (username === "admin" && password === "super_secret_password") {
+        console.log("Login successful!");
+    } else {
+        console.log("Login failed!");
+    }
+}
+
+// Calling the function
+login("admin", "my_password");
+
+// Secret: api_token
+const api_token = "du2f4927gfbfyf";
+console.log("API Token: " + api_token);
